08d1b5970e47c57192ac0650c898c99155491f31
Lili-Kiwi <bill2017bill2017@gmail.com>
2025-08-09 23:18:22 -0400

    Add: Conditional Rendering a Message, Mark Todo as Complete, Convert Form to Controlled Component, Disable Form Button when Input is Empty

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,19 +1,12 @@
 import { useState, useRef } from 'react';
 
 function TodoForm({ addTodo }) {
-  const [input, setInput] = useState('');
   const inputRef = useRef(null);
-  console.log("input => ", input);
-
+  const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("e.target.todoTitle => ", e.target.todoTitle);
-    console.log("e.target.todoTitle.value => ", e.target.todoTitle.value);
-    const todoTitle = e.target.todoTitle.value;
-    console.log("todoTitle => ", todoTitle);
-    addTodo(todoTitle);
-    // e.target.reset();
-    setInput('');
+    addTodo(workingTodoTitle);
+    setWorkingTodoTitle('');
     inputRef.current.focus();
   };
 
@@ -21,12 +14,14 @@ function TodoForm({ addTodo }) {
     <form onSubmit={handleSubmit}>
       <label htmlFor="todoTitle">Todo</label>
       <input
-        id="todoTitle"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
+        id="workingTodoTitle"
+        value={workingTodoTitle}
+        onChange={(e) => setWorkingTodoTitle(e.target.value)}
         ref={inputRef}
       />
-      <button type="submit">Add Todo</button>
+      <button type="submit" disabled={!workingTodoTitle}>
+        Add Todo
+      </button>
     </form>
   );
 }
