9322109129bf8c57d542d709a13c76d85dd90545
Lili-Kiwi <bill2017bill2017@gmail.com>
2025-09-07 16:42:57 -0400

    Debouncing Filter Input

---
+++ src/features/TodosViewForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const TodosViewForm = ({
+  sortDirection,
+  setSortDirection,
+  sortField,
+  setSortField,
+  queryString,
+  setQueryString,
+}) => {
+  const [localQueryString, setLocalQueryString] = useState(queryString);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      setQueryString(localQueryString);
+    }, 500);
+    return () => clearTimeout(debounce);
+  }, [localQueryString, setQueryString]);
+
+  return (
+    <form onSubmit={preventRefresh}>
+      <div>
+        <label>Search todos:</label>
+        <input
+          type="text"
+          value={localQueryString}
+          onChange={(e) => setLocalQueryString(e.target.value)}
+        />
+        <button type="button" onClick={() => setQueryString('')}>
+          Clear
+        </button>
+      </div>
+      <div>
+        <label>Sort by</label>
+        <select
+          value={sortField}
+          onChange={(e) => setSortField(e.target.value)}
+        >
+          <option value="title">Title</option>
+          <option value="createdTime">Time added</option>
+        </select>
+        <label>Direction</label>
+        <select
+          value={sortDirection}
+          onChange={(e) => setSortDirection(e.target.value)}
+        >
+          <option value="asc">Ascending</option>
+          <option value="desc">Descending</option>
+        </select>
+      </div>
+    </form>
+  );
+};
+const preventRefresh = (e) => {
+  e.preventDefault();
+};
+
+export default TodosViewForm;
